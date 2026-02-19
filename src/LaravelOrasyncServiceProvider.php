acb7c6d3e0d365b0d16fa2ad7902e0c194fc365f
Fathul Amin <mfathulamin2000@gmail.com>
2026-02-19 11:55:52 +0800

    package: bootstrap Laravel package skeleton
    Initialize Composer library with PSR-4 autoloading and Laravel service provider auto-discovery.
    
    Set compatibility to PHP ^8.1 and illuminate/support ^10|^11|^12.

---
+++ src/LaravelOrasyncServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Fathulamin2000\LaravelOrasync;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelOrasyncServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        //
+    }
+}
